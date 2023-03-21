ea73d01d02c1709acd7b543e979a15cc3feca10c
Mateo <mateomrvelj99@gmail.com>
2023-03-21 21:32:19 +0100

    export as pypi module

--- vt_python/setup.py
+++ vt_python/setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name="vtpython",
     version="1.2",
     author="Mateo Mrvelj",
-    author_email="mateomrvelj99@gmail.com",
+    author_email="burningmalwareblog@gmail.com",
     description="A very simple Python package for submitting files to VirusTotal for analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
