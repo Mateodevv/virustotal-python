4efb8e42a53d9161cd5da42df7bd445cda8fee5b
Mateo <mateomrvelj99@gmail.com>
2023-03-21 23:17:55 +0100

    export as pypi module

--- setup.py
+++ setup.py
@@ -5,17 +5,29 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="vtpython",
-    version="2.2",
+    version="2.6",
     author="Mateo Mrvelj",
     author_email="burningmalwareblog@gmail.com",
     description="A very simple Python package for submitting files to VirusTotal for analysis",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Mateodevv/virustotal-python",
-    packages=setuptools.find_packages(),
+    packages=['vtpython'],
+    py_modules=['vtpython'],
     python_requires='>=3.6',
     install_requires=[
         "requests>=2.25.1"
-    ]
+    ],
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 
 )
